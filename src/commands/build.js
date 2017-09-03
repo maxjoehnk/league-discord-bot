85b79f8cf4f6d4652c241abfe8a09a9416d163ff
Max Jöhnk <maxjoehnk@gmail.com>
2017-09-03 13:04:52 +0200

    improve league build message formatting

--- src/commands/build.js
+++ src/commands/build.js
@@ -7,15 +7,20 @@ const exec = config => async(args, msg) => {
     const champion = await riot.champions.find(db, config.keys.riot, query);
     const items = await riot.items.all(db, config.keys.riot);
     const stats = await championgg.champion(config.keys['champion.gg'], champion.id);
-    const message = buildMessage(champion, stats.hashes.skillorderhash.highestCount.hash, stats.hashes.skillorderhash.highestWinrate.hash);
+    const message = buildMessage(champion, stats.hashes.skillorderhash.highestCount, stats.hashes.skillorderhash.highestWinrate);
     msg.channel.send({ embed: message });
 };
+
+const formatSkillOrder = skillOrder => 
+    skillOrder.hash.split('-')
+        .filter((_, i) => i > 0)
+        .join('>');
 
 const buildMessage = (champion, mostFrequent, highestWin) => new Discord.RichEmbed()
     .setTitle(champion.name)
     .setThumbnail(champion.imageUrl)
-    .addField(`Most Frequent`, mostFrequent)
-    .addField(`Highest Win %`, highestWin);
+    .addField(`Skill Order - Most Frequent (${mostFrequent.count} Games)`, formatSkillOrder(mostFrequent))
+    .addField(`Skill Order - Highest Win % (${highestWin.count} Games)`, formatSkillOrder(highestWin));
 
 const cmd = 'build';
 
