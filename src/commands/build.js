2d142703535bd1956479289038cecea5d24982e3
Max Jöhnk <maxjoehnk@gmail.com>
2017-09-03 18:32:21 +0200

    [feat] add support for champion roles

--- src/commands/build.js
+++ src/commands/build.js
@@ -3,10 +3,14 @@ const { riot, championgg } = require('league-api');
 const { db } = require('../db');
 
 const exec = config => async(args, msg) => {
-    const query = args.join(' ');
-    const champion = await riot.champions.find(db, config.keys.riot, query);
-    const stats = await championgg.champion(config.keys['champion.gg'], champion.id);
-    const message = buildMessage(champion, stats.hashes.skillorderhash.highestCount, stats.hashes.skillorderhash.highestWinrate);
+    const champion = await riot.champions.find(db, config.keys.riot, args[0]);
+    let stats;
+    if (args.length === 2) {
+        stats = await championgg.byRole(config.keys['champion.gg'], champion.id, args[1]);
+    }else {
+        stats = await championgg.mostPlayed(config.keys['champion.gg'], champion.id);
+    }
+    const message = buildMessage(champion, stats);
     msg.channel.send({ embed: message });
 };
 
@@ -15,11 +19,16 @@ const formatSkillOrder = skillOrder =>
         .filter((_, i) => i > 0)
         .join('>');
 
-const buildMessage = (champion, mostFrequent, highestWin) => new Discord.RichEmbed()
-    .setTitle(champion.name)
-    .setThumbnail(champion.imageUrl)
-    .addField(`Skill Order - Most Frequent (${mostFrequent.count} Games)`, formatSkillOrder(mostFrequent))
-    .addField(`Skill Order - Highest Win % (${highestWin.count} Games)`, formatSkillOrder(highestWin));
+const buildMessage = (champion, stats) => {
+    const { highestCount, highestWinrate } = stats.hashes.skillorderhash;
+    const role = championgg.getLabelForRole(stats.role);
+    return new Discord.RichEmbed()
+        .setTitle(champion.name)
+        .setDescription(role)
+        .setThumbnail(champion.imageUrl)
+        .addField(`Skill Order - Most Frequent (${highestCount.count} Games)`, formatSkillOrder(highestCount))
+        .addField(`Skill Order - Highest Win % (${highestWinrate.count} Games)`, formatSkillOrder(highestWinrate));
+};
 
 const cmd = 'build';
 
