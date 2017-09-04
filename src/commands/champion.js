91153e38347bf4da0fc7ae782bc0dcce0b1ded09
Max Jöhnk <maxjoehnk@gmail.com>
2017-09-04 11:31:01 +0200

    [feat] add more metadata to champion command

--- src/commands/champion.js
+++ src/commands/champion.js
@@ -10,13 +10,22 @@ const exec = config => async(args, msg) => {
 };
 
 const buildMessage = champion => new Discord.RichEmbed()
-    .setTitle(champion.name)
-    .setDescription(champion.title)
-    .setThumbnail(champion.imageUrl)
-    .addField(`Q *${champion.skills[0].name}*`, champion.skills[0].description)
-    .addField(`W *${champion.skills[1].name}*`, champion.skills[1].description)
-    .addField(`E *${champion.skills[2].name}*`, champion.skills[2].description)
-    .addField(`R *${champion.skills[3].name}*`, champion.skills[3].description);
+    .setAuthor(champion.name, champion.imageUrl)
+    .setTitle(champion.title.slice(0, 1).toUpperCase() + champion.title.slice(1))
+    .setDescription(champion.loreExcerpt)
+    .addField('Health', `${champion.healthBase} (+${champion.healthPerLevel})`, true)
+    .addField('Health Regen', `${champion.healthRegenBase} (+${champion.healthRegenPerLevel})`, true)
+    .addField('Attack Damage', `${champion.adBase} (+${champion.adPerLevel})`, true)
+    .addField('Attack Speed', `${champion.asBase} (+${champion.asPerLevel}%)`, true)
+    .addField('Armor', `${champion.armorBase} (+${champion.armorPerLevel})`, true)
+    .addField('Magic Resist', `${champion.mrBase} (+${champion.mrPerLevel})`, true)
+    .addField('Movespeed', `${champion.movespeed}`, true)
+    .addField(`P ${champion.skills[0].name}`, champion.skills[0].description)
+    .addField(`Q ${champion.skills[1].name}`, champion.skills[1].description)
+    .addField(`W ${champion.skills[2].name}`, champion.skills[2].description)
+    .addField(`E ${champion.skills[3].name}`, champion.skills[3].description)
+    .addField(`R ${champion.skills[4].name}`, champion.skills[4].description)
+    .setImage(`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.key}_0.jpg`);
 
 const cmd = 'champion';
 
